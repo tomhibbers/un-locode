1d7d64718fb96b450474127883a6f6dce222dfb4
Tom Hibbers <tom.hibbers@entelect.co.za>
2018-09-26 15:55:48 +0200

    adding travis yml

---
+++ UNLOCODE.Tests/RepositoryTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace UNLOCODE.Tests
+{
+    public class RepositoryTest
+    {
+        public RepositoryTest()
+        {
+
+        }
+    }
+}
