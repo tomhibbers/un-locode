851c8219b7270023d26fb5fab916d9286f1db75e
Tom Hibbers <tom.hibbers@entelect.co.za>
2018-10-08 10:13:49 +0200

    added unit testt

--- UNLOCODE.Tests/RepositoryTest.cs
+++ UNLOCODE.Tests/RepositoryTest.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using System.Linq;
+
 namespace UNLOCODE.Tests
 {
     public class RepositoryTest
-    {
-        public RepositoryTest()
+    {        
+        [Fact]
+        public void UNLOCODE_Contain()
         {
-
+            var query = UNLOCODE.Repositories.Repository.UNLOCODEs.Where(x => x.Name.Contains("South"));
+            Assert.NotNull(query);
+            Assert.NotEmpty(query);
         }
     }
 }
